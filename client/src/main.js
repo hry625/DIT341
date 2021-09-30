9965f2eab6b1c7baa60427651584d143ffc8b019
flyf <hry87625@gmail.com>
2021-09-30 12:31:47 +0200

    add vuetify to frontend

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
+// import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify'
 
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')

85daa766e66444f20e73f480d8abe352100e5dfb
flyf <hry87625@gmail.com>
2021-09-30 12:47:53 +0200

    add firebase configuration

--- client/src/main.js
+++ client/src/main.js
@@ -6,10 +6,19 @@ import router from './router'
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify'
-
+import firebase from 'firebase'
 // Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+firebase.initializeApp({ apiKey: 'AIzaSyCyyuISTGS8m-6WWsi-m_o2bPSNFa8C-MY',
+authDomain: 'sharedcalendar-f9ae0.firebaseapp.com',
+projectId: 'sharedcalendar-f9ae0',
+storageBucket: 'sharedcalendar-f9ae0.appspot.com',
+messagingSenderId: '326352711813',
+appId: '1:326352711813:web:0c0b5018c488b28ccc39ec',
+measurementId: 'G-Y3CZHN6500'
+})
+
 
 new Vue({
   router,

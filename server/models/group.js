e8207102a08b16ae2762191de81ce871c5944df3
flyf <hry87625@gmail.com>
2021-09-30 18:30:09 +0200

    add group model to backend

---
+++ server/models/group.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var chatSchema = require('./chat').chatSchema
+
+var groupSchema = new Schema({
+    chats:[chatSchema]
+
+})
+
+module.export = mongoose.model('group',groupSchema);

e3b535ed254d03dbf1c7fc27acab1e264a7b1d83
flyf <hry87625@gmail.com>
2021-10-07 00:47:10 +0200

    add firebase and vuex to the calendar

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
+import Vuex from 'vuex'
 
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify'
-
-// Vue.use(BootstrapVue)
+import firebase from 'firebase/compat/app'
+Vue.use(BootstrapVue)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCyyuISTGS8m-6WWsi-m_o2bPSNFa8C-MY',
+    authDomain: 'sharedcalendar-f9ae0.firebaseapp.com',
+    projectId: 'sharedcalendar-f9ae0',
+    storageBucket: 'sharedcalendar-f9ae0.appspot.com',
+    messagingSenderId: '326352711813',
+    appId: '1:326352711813:web:0c0b5018c488b28ccc39ec',
+    measurementId: 'G-Y3CZHN6500'
+  })
+}
 
 new Vue({
   router,

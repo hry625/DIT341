613439074a944a1b4000827a0e2092fda63f8e69
flyf <hry87625@gmail.com>
2021-10-11 13:26:41 +0200

    add sign in logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,62 +1,84 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-navigation-drawer absolute temporary v-model="drawerToggle">
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            Online Users {{ onlineUsers[0] }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-list>
+        <v-list-item
+          avatar
+          v-for="user in onlineUsers[1]"
+          v-bind:key="user.user.username"
+        >
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ user.user.username }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app class="light-blue darken-1">
+      <v-app-bar-nav-icon
+        @click.native.stop="drawerToggle = !drawerToggle"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link to="/chat/0" tag="span" style="cursor: pointer"
+          >Vuetify Chat</router-link
+        >
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items v-for="item in menuItems" v-bind:key="item.route">
+        <v-btn text :key="item.title" :to="item.route">
+          <v-icon left>{{ item.icon }}</v-icon>
+          <div class="hidden-xs-only">{{ item.title }}</div>
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld />
-      <!--<Calendar />-->
-     <Calendar />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-// import Calendar from './components/SharedCalendar'
-import Calendar from './components/Calendar.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-    // Calendar,
-    Calendar
+  data() {
+    return {
+      drawerToggle: false
+    }
   },
-
-  data: () => ({
-    //
-  })
+  computed: {
+    menuItems() {
+      let items = [
+        { icon: 'mdi-face', title: 'Register', route: '/register' },
+        { icon: 'mdi-lock-open', title: 'Login', route: '/login' }
+      ]
+      if (this.userIsAuthenticated) {
+        items = [
+          { icon: 'mdi-forum', title: 'Create a Chat', route: '/create' },
+          { icon: 'mdi-chat', title: 'Chat List', route: '/discover' }
+        ]
+      }
+      return items
+    },
+    userIsAuthenticated() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      )
+    },
+    onlineUsers() {
+      return this.$store.getters.onlineUsers
+    }
+  }
 }
 </script>

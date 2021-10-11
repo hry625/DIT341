daa122923cfcbb2912d5c9a2d2156b5484704d80
flyf <hry87625@gmail.com>
2021-10-11 13:25:11 +0200

    update main js

--- client/src/main.js
+++ client/src/main.js
@@ -1,32 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import Vuex from 'vuex'
+// import BootstrapVue from 'bootstrap-vue'
+// import Vuex from 'vuex'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify'
 import firebase from 'firebase/compat/app'
-Vue.use(BootstrapVue)
-Vue.use(Vuex)
+import { store } from './store'
+import AlertComponent from './components/Shared/Alert.vue'
+// Vue.use(BootstrapVue)
+Vue.component('app-alert', AlertComponent)
 
 Vue.config.productionTip = false
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyCyyuISTGS8m-6WWsi-m_o2bPSNFa8C-MY',
-    authDomain: 'sharedcalendar-f9ae0.firebaseapp.com',
-    projectId: 'sharedcalendar-f9ae0',
-    storageBucket: 'sharedcalendar-f9ae0.appspot.com',
-    messagingSenderId: '326352711813',
-    appId: '1:326352711813:web:0c0b5018c488b28ccc39ec',
-    measurementId: 'G-Y3CZHN6500'
-  })
-}
+// if (!firebase.apps.length) {
+//   firebase.initializeApp({
+//     apiKey: 'AIzaSyCyyuISTGS8m-6WWsi-m_o2bPSNFa8C-MY',
+//     authDomain: 'sharedcalendar-f9ae0.firebaseapp.com',
+//     projectId: 'sharedcalendar-f9ae0',
+//     storageBucket: 'sharedcalendar-f9ae0.appspot.com',
+//     messagingSenderId: '326352711813',
+//     appId: '1:326352711813:web:0c0b5018c488b28ccc39ec',
+//     measurementId: 'G-Y3CZHN6500'
+//   })
+// }
 
 new Vue({
   router,
   vuetify,
-  render: function (h) { return h(App) }
+  store,
+  render: function (h) { return h(App) },
+  created() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCyyuISTGS8m-6WWsi-m_o2bPSNFa8C-MY',
+      authDomain: 'sharedcalendar-f9ae0.firebaseapp.com',
+      databaseURL: 'https://sharedcalendar-f9ae0-default-rtdb.europe-west1.firebasedatabase.app',
+      projectId: 'sharedcalendar-f9ae0',
+      storageBucket: 'sharedcalendar-f9ae0.appspot.com',
+      messagingSenderId: '326352711813',
+      appId: '1:326352711813:web:0c0b5018c488b28ccc39ec',
+      measurementId: 'G-Y3CZHN6500'
+    })
+  }
 }).$mount('#app')

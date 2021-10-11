8d37dbb8a1f0a86f82955bafaede267f1917c03b
flyf <hry87625@gmail.com>
2021-10-11 13:20:09 +0200

    change router file to router folder

---
+++ client/src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../components/Home.vue'
+import Chat from '../components/Chat/Chat.vue'
+import Create from '../components/Chat/Create.vue'
+import ChatList from '../components/Chat/ChatList.vue'
+import Profile from '../components/User/Profile.vue'
+import Signup from '../components/User/Signup.vue'
+import Signin from '../components/User/Signin.vue'
+import AuthGuard from './auth-guard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+      beforeEnter: AuthGuard
+    },
+    {
+      path: '/login',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/register',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/chat/:id',
+      name: 'Chat',
+      component: Chat,
+      props: true,
+      beforeEnter: AuthGuard
+    },
+    {
+      path: '/create',
+      name: 'CreateChat',
+      component: Create,
+      beforeEnter: AuthGuard
+    },
+    {
+      path: '/discover',
+      name: 'JoinChat',
+      component: ChatList,
+      beforeEnter: AuthGuard
+    }
+  ],
+  mode: 'history'
+})

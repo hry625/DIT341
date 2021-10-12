7b4a7e9e84401c52e1b6516e709f30de753ea87f
flyf <hry87625@gmail.com>
2021-10-12 16:53:48 +0200

    remove password attribute from the backend user schema

--- server/models/user.js
+++ server/models/user.js
@@ -13,9 +13,9 @@ var userSchema = new Schema({
   },
   username: {
     type: String
-  },
-  password: {
-    type: String
   }
+  // password: {
+  //   type: String
+  // }
 });
 module.exports = mongoose.model('user', userSchema);

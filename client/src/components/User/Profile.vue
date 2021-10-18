f2dc41823fc49f288eb26aa533acafdba84da3ff
astridbe <astridbe@student.chalmers.se>
2021-10-18 15:30:20 +0200

    add error handling when deleting user

--- client/src/components/User/Profile.vue
+++ client/src/components/User/Profile.vue
@@ -147,6 +147,14 @@ export default {
         })
         .catch((error) => {
           this.message = error
+          if (error.response) {
+            alert(
+              'Oh no something went wrong when deleting user, Status code ' +
+                error.response.status
+            )
+          } else {
+            alert('Oops something went wrong when deleting user')
+          }
         })
     }
   },

9488206d2b0b3e378cf4efd57737fe1c14143da4
olgara <olgara@student.chalmers.se>
2021-10-19 20:27:49 +0200

    edit user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,7 +33,7 @@ router.post("/api/users", async function (req, res) {
     //   return User + 1;
     // };
 
-    let newUser = new User({
+    let newUser = new user({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
@@ -106,7 +106,7 @@ router.delete("/api/users", function (req, res, next) {
 
 //edit user 
 router.put("/api/users/:email", function (req, res, next) {
-  const email = req.params.email;
+  var email = req.params.email;
   // console.log(req.body)
   user.findOneAndUpdate(email, req.body, (err, user) => {
     if (err) {
@@ -130,7 +130,7 @@ router.patch("/api/users/:email", function (req, res, next) {
 // find userEmail
 router.get('/api/users/:email', function(req, res, next) {
   var email = req.params.email;
-  User.findOne({email}, function(err, user) {
+  user.findOne({email}, function(err, user) {
       if (err) { return next(err); }
       console.log(user)
       res.status(201).json(user);
